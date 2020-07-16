8c227a6f6bfb12faeb9ab03a90d2d48c6127ca00
azevedorafael <you@example.com>
2020-07-16 17:59:04 -0300

    Display NProgress in route changes

--- frontend/components/Header/index.js
+++ frontend/components/Header/index.js
@@ -1,8 +1,22 @@
 import Link from "next/link";
+import Router from "next/router";
+import NProgress from "nprogress";
 
 import Nav from "../Nav";
 
 import * as S from "./styles";
+
+Router.onRouteChangeStart = () => {
+  NProgress.start();
+};
+
+Router.onRouteChangeComplete = () => {
+  NProgress.done();
+};
+
+Router.onRouteChangeError = () => {
+  NProgress.done();
+};
 
 const Header = () => (
   <S.StyledHeader>
